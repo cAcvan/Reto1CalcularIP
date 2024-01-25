28462ca5a0095bfe2539105ae6aee64f98894c1f
Ivan <153597518+cAcvan@users.noreply.github.com>
2024-01-25 10:43:49 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public static void main(String[] args) throws InterruptedException {
-    Marco m = new Marco();
+    Validacion m = new Marco();
     Scanner sc = new Scanner(System.in);
     int[] array = new int[4];
     int[] array2 = new int[4];
